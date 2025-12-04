b73370dea74a4d1c4363bf3177c7ecc66314efc8
BurntBread007 <brennan.ramsey@outlook.com>
2025-12-04 11:07:57 -0500

    Add data
    Filled out data segment with text and game variables.

--- main.asm
+++ main.asm
@@ -5,11 +5,23 @@ INCLUDE Irvine32.inc
 ExitProcess proto, dwExitCode:dword
 
 .data
-; data goes here
+; Game Variables
+grid BYTE 9 DUP (?) ; 3x3 grid of ?'s
+player BYTE 'X'
+computer BYTE 'O'
+
+; Text
+playerTurnMsg BYTE "Player's turn. Use WASD to move and Space to confirm.",0
+playerName BYTE "Player",0
+computerName BYTE "Computer",0
+winnerMsg BYTE " wins!",0
+tieMsg BYTE "It's a tie!",0
 
 .code
-
+;------------------------------------------
 main PROC
+; Description: Entry point - starts the Tic Tac Toe game
+;------------------------------------------
     ; code goes here
     invoke ExitProcess, 0
 
